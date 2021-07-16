f2207a5f9aad394feea004d5727fa85290cfe740
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-07-16 18:58:28 +0200

    delete list by name

--- server.js
+++ server.js
@@ -4,16 +4,18 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json())
-const data = require('./data');
+let data = require('./data');
 
 app.get('/', (req, res) => {
   res.json(`route /lists to fetch all list names`)
 });
 
+// Get All Lists
 app.get('/lists', (req, res) => {
   res.json(data.map(el => el.name));
 });
 
+// Get List by Name
 app.get('/lists/:name', (req, res) => {
   const { name } = req.params;
   const nameFound = data.some(el => el.name === name);
@@ -22,6 +24,20 @@ app.get('/lists/:name', (req, res) => {
     res.json(filteredName);
   } else {
     res.status(404).json({msg: `No list with the name of ${name}`});
+  }
+});
+
+// Delete List by Name
+app.delete('/lists/:name', (req, res) => {
+  const { name } = req.params;
+  const nameFound = data.some(el => el.name === name);
+
+  if (nameFound) {
+    const deletedNames = data.filter(el => el.name !== name);
+    data = deletedNames;
+    res.json({ msg: `List with name of ${name} deleted!` });
+  } else {
+    res.status(404).json({msg: `List with the name of ${name} not found!`});
   }
 });
 
