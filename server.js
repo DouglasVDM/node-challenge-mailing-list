f78b5998ef07a4a73d19ef3836fa32ba33b83ab9
DouglasVDM <74470226+DouglasVDM@users.noreply.github.com>
2021-07-16 18:45:05 +0200

    get a single list by name

--- server.js
+++ server.js
@@ -14,6 +14,17 @@ app.get('/lists', (req, res) => {
   res.json(data.map(el => el.name));
 });
 
+app.get('/lists/:name', (req, res) => {
+  const { name } = req.params;
+  const nameFound = data.some(el => el.name === name);
+  if (nameFound) {
+    const filteredName = data.filter(el => el.name === name);
+    res.json(filteredName);
+  } else {
+    res.status(404).json({msg: `No list with the name of ${name}`});
+  }
+});
+
 app.listen(PORT, (req, res) => {
   console.log(`Server started on port:${PORT}`);
 });
